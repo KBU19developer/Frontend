c1fd3725ea1ada176856e57aabbbf1e57fce45c9
Leehogyu <59430391+JamongFriend@users.noreply.github.com>
2023-03-07 16:23:54 +0900

    Add files via upload

---
+++ src/pages/aboutBoard/ViewPosting/posting.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Button from '../../../functions/BackButton';
+
+function Posting(){
+    const [value, setValue] = useState('');
+    const callnum = useParams().postnum;
+    const home = useNavigate();
+    useEffect(() => {axios.get(`/Board/postnum/${callnum}`)
+    .then(result => {
+        setValue(result.data);
+    })}, []);
+
+    return (
+        <div>
+            <header>
+                <h2>{value[0]}</h2>
+                writer : {value[2]}<br />
+                <hr />
+            </header>
+            <main>
+                <pre>
+                    {value[1]}
+                </pre>
+                <br />
+                time : {value[3]}
+            </main>
+            <footer>
+                <Button path="/Board/1" name="Back" />
+            </footer>
+        </div>
+    );
+}
+
+export default Posting;
